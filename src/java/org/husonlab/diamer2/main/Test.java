8e8c2f2a6149e06859cb6a27e07f1aa6c575ff03
Noel Kubach <nk035175@gmail.com>
2024-11-08 22:24:54 +0100

    Exported jar to run on the server.

--- src/java/org/husonlab/diamer2/main/Test.java
+++ src/java/org/husonlab/diamer2/main/Test.java
@@ -3,11 +3,11 @@ package org.husonlab.diamer2.main;
 public class Test {
     public static void main(String[] args) {
         TestParallel testParallel = new TestParallel(
-                16,
-                1,
-                100,
+                Integer.valueOf(args[0]),   // Threads
+                Integer.valueOf(args[1]),   // max queue size
+                Integer.valueOf(args[2]),   // fasta batch size
                 new short[]{1, 10},
-                "F:\\Studium\\Master\\semester5\\thesis\\data\\NCBI\\reduced\\nr100.fsa");
+                args[3]);                   // path to fsa
         try {
             testParallel.run(args);
         } catch (Exception e) {
